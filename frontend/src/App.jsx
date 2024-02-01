2bd3d4360b4aa1ca0d7fb373d35ba060562daf99
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-02-01 13:54:45 +0530

    Create Header Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
