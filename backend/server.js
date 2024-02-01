6d001b624a5cee1888fa56661a109707792b9e19
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-02-01 22:11:49 +0530

    Create test api route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import dbConnection from './dbConfig/dbConnection.js';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 dbConnection();
 
 app.listen(3000, () => {
    console.log('Server is running on http://localhost:3000');
 });
+
+app.use('/api/user', userRoutes);
