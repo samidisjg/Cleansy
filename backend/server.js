2dfb0b0c2d52e546facb7c288519f18c6169d80e
Ramindu Nimesh <117975788+ramindu-Nimex@users.noreply.github.com>
2024-02-01 23:23:10 +0530

    Create signup API route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import dbConnection from './dbConfig/dbConnection.js';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -15,3 +16,4 @@ app.listen(3000, () => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
