e891a1637037b66925f40839dadcb246123d32b5
samidisjg <samidisjg@gmail.com>
2024-03-15 18:28:00 +0530

    dashboard changes

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,36 +5,43 @@ import DashProfile from "../components/DashProfile";
 import DashUsers from "../components/DashUsers";
 import DashProperties_02 from "../components/IT22577160_Components/DashProperties_02";
 import DashServices from "../components/IT22350114_Components/DashServices";
+import DashPayments_03 from './../components/IT22602978_Components/DashPayments_03';
+import DashUserPayments_03 from './../components/IT22602978_Components/DashUserPayments_03';
+import AddPaymentProfile from './IT22602978_Pages/AddPaymentProfile';
 
 
 const Dashboard = () => {
   const location = useLocation();
-  const [tab, setTab] = useState("");
+  const [tab, setTab] = useState('')
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromUrl = urlParams.get("tab");
-    if (tabFromUrl) {
-      setTab(tabFromUrl);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl) {
+      setTab(tabFromUrl)
     }
-  }, [location.search]);
+  }, [location.search])
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
         {/* Sidebar */}
-        <DashSidebar />
+        <DashSidebar/>
       </div>
       {/* profile */}
-      {tab === "profile" && <DashProfile />}
+      {tab === 'profile' && <DashProfile/>}
       {/* profile */}
-      {tab === "properties" && <DashProperties_02 />}
+      {tab === 'properties' && <DashProperties_02 />}
       {/* users */}
-      {tab === "users" && <DashUsers />}
+      {tab === 'users' && <DashUsers/>}
       {/* services */}
-      {tab === "services" && <DashServices />}
-
-  
+      {tab === 'services' && <DashServices />}
+      {/* payments */}
+      {tab === 'payments' && <DashPayments_03/>}
+      {/* user payments */}
+      {tab === 'userpayments' && <DashUserPayments_03/>}
+      {/* add payments */}
+      {tab === 'addpayments' && <AddPaymentProfile/>}
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
