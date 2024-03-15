21dadbc0933b823aa5b355488d48ac03f391686f
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-03-15 17:37:53 +0530

    Complete show apartment listing functionality

--- frontend/src/components/IT22577160_Components/DashApartmentList_02.jsx
+++ frontend/src/components/IT22577160_Components/DashApartmentList_02.jsx
@@ -1,8 +1,85 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { MdLocationOn, MdEdit } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+
 
 const DashApartmentList_02 = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [apartmentListing, setApartmentListing] = useState([]);
+  const [showListingError, setShowListingError] = useState(false);
+
+  useEffect(() => {
+    const fetchApartmentListing = async () => {
+      try {
+        setShowListingError(false);
+        const res = await fetch(`/api/apartmentListing/apartmentListings/${currentUser._id}`)
+        const data = await res.json()
+        if(data.success === false) {
+          setShowListingError(true);
+          return;
+        }
+        setApartmentListing(data)
+      } catch (error) {
+        setShowListingError(true);
+      }
+    }
+    fetchApartmentListing()
+  }, [currentUser._id]);
+
+  console.log(apartmentListing);
+
   return (
-    <div>DashApartmentList_02</div>
+    <div className='max-full px-3 mt-6 mx-auto'>
+      <h1 className='text-center my-7 font-extrabold text-3xl underline'>Your Apartment Listing</h1>
+      <p>{showListingError ? "Something Went Wrong About Your Apartment Listing" : ""}</p>
+      {
+        apartmentListing && apartmentListing.length > 0 ? (
+          <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
+            {
+              apartmentListing.map((listing) => (
+                <>
+                  <div key={listing._id}>
+                      <li className="relative bg-white dark:bg-slate-700 flex flex-col justify-between items-center shadow-lg hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-150 m-[10px]">
+                        <Link className="content" to={`/apartmentListing/${listing._id}/`}>
+                          <img src={listing.imageUrls[0]} alt="listing cover" className='h-52 w-full object-cover'/>
+                          
+                          <div className="w-full p-[10px]">
+                            <div className="flex items-center space-x-1">
+                              <MdLocationOn className="h-4 w-4 text-green-500" />
+                              <p >{listing.blockNumber}</p>
+                            </div>
+                            <p className="font-semibold text-xl text-slate-700 dark:text-slate-200 truncate">{listing.ownerName}</p>
+                            <p className="font-semibold text-sm text-slate-400">{listing.ownerContactNumber}</p>
+                            <p className="text-[#457b9d] mt-2 font-semibold">${listing.offer ? listing.discountPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} {listing.type === "rent" && " / month"}</p>
+                            <div className="flex items-center mt-[10px] space-x-3">
+                              <div className="font-bold text-xs">
+                                <p >{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}</p>
+                              </div>
+                              <div className="font-bold text-xs">
+                                <p >{listing.bathrooms > 1 ? `${listing.bathrooms} Baths` : "1 Bath"}</p>
+                              </div>
+                            </div>
+                          </div>
+                        </Link>
+                        <FaTrash className="absolute bottom-2 right-2 text-red-500 hover:text-red-700 transition ease-in-out duration-200 cursor-pointer h-[14px]" />
+                        <Link to={`/update-apartmentListing/${listing._id}`}>
+                          <MdEdit className="absolute bottom-2 right-7 text-green-500 hover:text-green-700 transition ease-in-out duration-200 h-4" />
+                        </Link>
+                      </li>
+                  </div>
+                </>
+              ))
+  
+            }
+          </ul>
+          )
+           : (
+          <p className='text-center font-mono'>You have no apartment listings</p>
+        )
+      }
+    </div>
   )
 }
 
