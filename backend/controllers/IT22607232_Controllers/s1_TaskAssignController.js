1183fa198d290cc78161baefda769033059995d1
samidisjg <samidisjg@gmail.com>
2024-03-17 01:59:19 +0530

    crud completion

--- backend/controllers/IT22607232_Controllers/s1_TaskAssignController.js
+++ backend/controllers/IT22607232_Controllers/s1_TaskAssignController.js
@@ -9,27 +9,91 @@ export const TaskAssigning = async (req, res, next) => {
       return res.status(404).json({ msg: "Task Assigning failed" });
     }
     return res
-      .status(201)
+      .status(200)
       .json(newTaskAssign, { msg: "Task Assigned Successfully" });
   } catch (error) {
     next(error);
   }
 };
 
-//Get ALL Task Assigning
+//Get ALL Task Assiged
 export const allTasks = async (req, res, next) => {
-  if (req.User.TaskID === req.params.id || req.User.isAdmin) {
     try {
-      const Scheduling = await TaskAssign.find({TaskID: req.params.TaskID});
+      const Scheduling = await TaskAssign.find();
       if (!Scheduling) {
-        res.status(404).json({ msg: "Task not found" });
+        res.status(404).json({ msg: "Tasks not found" });
       }
       res.status(200).json(Scheduling);
     } catch (error) {
       next(error);
       res.status(500).json({ error: error });
     }
+};
+
+
+//get One Task by TaskID
+export const oneTask = async (req, res,next) => {
+  try {
+    let taskid = req.params.taskid;
+    const taskOne = await TaskAssign.findOne({TaskID: taskid});
+
+    if (!taskOne) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json(taskOne);
+  } catch (error) {
+    console.error("Error retrieving task:", error);
+    next(error);
+    return res.status(500).json({ error: "Internal server error" });
+   
+  }
+};
+
+/*//Update course by Admin
+export const updateTask = async (req,res,next)=>{
+  try{
+    TaskAssign.findByIdAndUpdate({ taskid: req.params.taskid }, updateTask).then(result => {
+      console.log(result);
+      res.status(200).json({ message: "Successfully Task Updated" })
+  })
+}
+catch (error) {
+  console.error("Error retrieving task:", error);
+  next(error);
+  return res.status(500).json({ error: "Internal server error" });
+ 
+}
+}*/
+
+// Update Task by Facility manager
+export const updateTask = async (req, res, next) => {
+  try {
+    const taskId = req.params.taskid;
+    const updateTaskData = req.body; //declaring updated data
+
+    const updatedTask = await TaskAssign.findByIdAndUpdate(taskId, updateTaskData, { new: true });
+
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json({ message: "Successfully Task Updated", updatedTask });
+  } catch (error) {
+    console.error("Error updating task:", error);
+    next(error); //pass error to error handling middleware
   }
 };
 
 
+//delete Tasks
+export const deleteTask = async(req,res,next)=>{
+  try{
+    await TaskAssign.findByIdAndDelete(req.params.taskid)
+    res.status(200).json({ message: "Successfully Deleted assigned task" })
+  }
+  catch (error){
+    return res.status(500).json({msg: error.message})
+  }
+}
+ 
+ 
