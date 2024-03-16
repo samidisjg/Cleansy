6db88b06633991c971131652996a350ac1804766
Thewa-AlienHub <117763807+Thewa-AlienHub@users.noreply.github.com>
2024-03-16 12:53:40 +0530

    correction1

--- backend/controllers/IT22602978_Controllers/PaymentProfileCreation.controller_03.js
+++ backend/controllers/IT22602978_Controllers/PaymentProfileCreation.controller_03.js
@@ -1,10 +1,18 @@
 import Addpayment from "../../models/IT22602978_Models/PaymentProfileCreation.model_03.js";
-
+import bcryptjs from "bcryptjs";
 
 export const CreatePaymentProfile = async (req, res, next) => {
+   const { OwnerId, ownerUsername, ownerhousenumber, password } = req.body;
 
+   const hashedPassword1 = bcryptjs.hashSync(password, 10);
+   const newAddpayment1 = new Addpayment({
+      OwnerId,
+      ownerUsername,
+      ownerhousenumber,
+      password: hashedPassword1,
+   });
    try {
-      const newaddpayment=await Addpayment.create(req.body);
+      const newaddpayment=await newAddpayment1.save();
       return res.status(201).json(newaddpayment);
       
    } catch (error) {
