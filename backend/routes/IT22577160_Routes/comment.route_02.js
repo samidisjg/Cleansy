f1c1a455dd39e02889720698349f9b4a66d70878
ramindu-Nimex <ramindunimesh61@gmail.com>
2024-03-31 23:00:47 +0530

    Add like functionality to the comment component

--- backend/routes/IT22577160_Routes/comment.route_02.js
+++ backend/routes/IT22577160_Routes/comment.route_02.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createComment, getPostComments, getUserComments } from '../../controllers/IT22577160_Controllers/comment.controller_02.js';
+import { createComment, getPostComments, getUserComments, likeComment } from '../../controllers/IT22577160_Controllers/comment.controller_02.js';
 import { verifyToken } from '../../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:resourceId', getPostComments);
 router.get('/getUserComments/:userId', getUserComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 export default router;
--- frontend/src/components/IT22577160_Components/Comment_02.jsx
+++ frontend/src/components/IT22577160_Components/Comment_02.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import moment from 'moment'
+import { FaThumbsUp } from 'react-icons/fa'
+import { useSelector } from 'react-redux';
 
-const Comment_02 = ({comment}) => {
+const Comment_02 = ({comment, onLike}) => {
    const [user, setUser] = useState({});
-   console.log(user);
+   const { currentUser } = useSelector((state) => state.user);
 
    useEffect(() => {
       const getUserComments = async () => {
@@ -33,6 +35,16 @@ const Comment_02 = ({comment}) => {
             </span>
          </div>
          <p className="text-gray-500 mb-2">{comment.content}</p>
+         <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+            <button type='button' onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}>
+               <FaThumbsUp className="text-sm" />
+            </button>
+            <p className="text-gray-400">
+               {
+                  comment.numberOfLikes > 0 && comment.numberOfLikes + ' ' + (comment.numberOfLikes === 1 ? 'like' : 'likes')
+               }
+            </p>
+         </div>
       </div>
     </div>
   )
