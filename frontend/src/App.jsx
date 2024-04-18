31f7f533aff73991cb4c9d96c6a724be3440b3c1
Pradicksha <pradicksha1230@gmail.com>
2024-04-18 11:49:30 +0530

    staff management report generate done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,46 +6,6 @@ import SignUp from "./pages/SignUp";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import Footer from './components/Footer'
-import Dashboard from './pages/Dashboard'
-import PrivateRoute from './components/PrivateRoute'
-import CreateApartmentListing from './pages/IT22577160_Pages/CreateApartmentListing'
-import UpdateApartmentListing_02 from './pages/IT22577160_Pages/UpdateApartmentListing_02'
-import DashMaintenance from './components/IT22607232_Components/DashMaintenance'
-import TaskAssign from './pages/IT22607232_Pages/s1_TaskAssignCreation'
-import RequestLeave_04 from './pages/IT22603418_Pages/RequestLeave_04'
-import OnlyPropertyAdminPrivateRoute_02 from './components/IT22577160_Components/OnlyPropertyAdminPrivateRoute_02'
-import CreateSharedResources_02 from './pages/IT22577160_Pages/CreateSharedResources_02'
-import UpdateSharedResources_02 from './pages/IT22577160_Pages/UpdateSharedResources_02'
-import SharedResourcesPage_02 from './pages/IT22577160_Pages/SharedResourcesPage_02'
-import ScrollToTop_02 from './components/IT22577160_Components/ScrollToTop_02'
-import MarketPlace from './pages/IT22577160_Pages/MarketPlace'
-import SearchResources_02 from './pages/IT22577160_Pages/SearchResources_02'
-import TasksTable_01 from './components/IT22607232_Components/TasksTable_01'
-import S1_UpdateTasks from './pages/IT22607232_Pages/s1_UpdateTasks'
-import ApartmentListingPage_02 from './pages/IT22577160_Pages/ApartmentListingPage_02'
-import SearchApartments_02 from './pages/IT22577160_Pages/SearchApartments_02'
-import CheckOutPage_02 from './pages/IT22577160_Pages/CheckOutPage_02'
-import AmenityCreate from './pages/IT22003546_Pages/AmenityCreate_05'      
-import AddVisitors from './pages/IT22561466_Pages/AddVisitors'
-import GamePage_02 from './pages/IT22577160_Pages/GamePage_02'
-import AmenityList_05 from './components/IT22003546_Components/AmenityList_05';
-import RatingWorkGroup_01 from './components/IT22607232_Components/RatingWorkGroup_01';
-import AmenityUpdate_05 from './pages/IT22003546_Pages/AmenityUpdate_05'
-import RateReview_01 from './pages/IT22607232_Pages/RateReview_01'
-import Team from './pages/Team'
-import Contact from './pages/Contact'
-import BoxTile from './components/IT22003546_Components/UserAmenityView'
-import AmenityDetails from './components/IT22003546_Components/AmenityDetailsEach_05'
-import BookAmenity from './pages/IT22003546_Pages/BookAmenity_05'
-import ServiceListingCreate from './pages/IT22350114_Pages/ServiceCreate_06'
-import BookingList_05 from './components/IT22003546_Components/BookingList_05'
-import BookingUpdate_05 from './pages/IT22003546_Pages/BookingUpdate_05'
-import DashServiceList_06 from './components/IT22350114_Components/DashServiceList_06'
-
-
-
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
@@ -74,9 +34,15 @@ import AmenityList_05 from "./components/IT22003546_Components/AmenityList_05";
 import RatingWorkGroup_01 from "./components/IT22607232_Components/RatingWorkGroup_01";
 import AmenityUpdate_05 from "./pages/IT22003546_Pages/AmenityUpdate_05";
 import RateReview_01 from "./pages/IT22607232_Pages/RateReview_01";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
 import BoxTile from "./components/IT22003546_Components/UserAmenityView";
 import AmenityDetails from "./components/IT22003546_Components/AmenityDetailsEach_05";
 import BookAmenity from "./pages/IT22003546_Pages/BookAmenity_05";
+import ServiceListingCreate from "./pages/IT22350114_Pages/ServiceCreate_06";
+import BookingList_05 from "./components/IT22003546_Components/BookingList_05";
+import BookingUpdate_05 from "./pages/IT22003546_Pages/BookingUpdate_05";
+import DashServiceList_06 from "./components/IT22350114_Components/DashServiceList_06";
 import RequestDetails_04 from "./pages/IT22603418_Pages/RequestDetails_04";
 
 function App() {
@@ -112,9 +78,15 @@ function App() {
               <Route path="/checkout" element={<CheckOutPage_02 />} />
               <Route path="/amenity-create" element={<AmenityCreate />} />
               <Route path="/add-visitors" element={<AddVisitors />} />
-                <Route path="/booking-List:bookingID" element={<BookingList_05/>} />
-                <Route path="/update-booking/:bookingID" element={<BookingUpdate_05/>} />
-                
+              <Route
+                path="/booking-List:bookingID"
+                element={<BookingList_05 />}
+              />
+              <Route
+                path="/update-booking/:bookingID"
+                element={<BookingUpdate_05 />}
+              />
+
               <Route
                 path="/book-amenity/:amenityId"
                 element={<BookAmenity />}
@@ -146,9 +118,15 @@ function App() {
               <Route
                 path="/update-sharedResourceListing/:resourceId"
                 element={<UpdateSharedResources_02 />}
-              />                <Route path="/service-create" element={<ServiceListingCreate/>} />
-                <Route path="/service-list:serviceID" element={<DashServiceList_06/>} />
-
+              />{" "}
+              <Route
+                path="/service-create"
+                element={<ServiceListingCreate />}
+              />
+              <Route
+                path="/service-list:serviceID"
+                element={<DashServiceList_06 />}
+              />
             </Route>
             <Route path="/projects" element={<Projects />} />
             <Route path="/create_04" element={<RequestLeave_04 />} />
@@ -203,8 +181,8 @@ function App() {
               path="/sharedResource/:resourceSlug"
               element={<SharedResourcesPage_02 />}
             />
-              <Route path='/team' element={<Team />}/>
-              <Route path='/contact' element={<Contact />}/>
+            <Route path="/team" element={<Team />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
         <Footer />
