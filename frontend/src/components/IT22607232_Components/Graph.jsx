377f506d1338c6781fd177cfec246410bd5ae704
Samidi Jayawickrama <107723505+samidisjg@users.noreply.github.com>
2024-04-19 20:04:04 +0530

    commit2

--- frontend/src/components/IT22607232_Components/Graph.jsx
+++ frontend/src/components/IT22607232_Components/Graph.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import {Doughnut} from 'react-chartjs-2';
-import {Chart,ArcElement} from 'chart.js';
-
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Chart, ArcElement } from "chart.js";
+import Labels from "./Labels.jsx";
 
 Chart.register(ArcElement);
 
@@ -18,42 +18,43 @@ Chart.register(ArcElement);
 };*/
 
 const config = {
-  data:{
-  datasets: [{
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
+  data: {
+    datasets: [
+      {
+        data: [300, 50, 100],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+        borderRadius: 30,
+        spacing: 10,
+      },
     ],
-    hoverOffset: 4,
-    borderRadius: 30,
-    spacing: 10
-  }]
-},
+  },
   options: {
-   cutout:115
-  }
-}
-
-
+    cutout: 115,
+  },
+};
 
 export default function Graph() {
   return (
-    <div className='flex-justify-content max-w-xs mx-auto'>
+    <div className="flex-justify-content max-w-xs mx-auto">
       <div className="item">
         <div className="chart-relative">
-        <Doughnut {...config}></Doughnut>
-       
-        <h3 className='mb-4 font-bold title'>Total Tasks
-        <span className='block text-3xl text-emerald-400'>#(0)</span>
-        </h3>
-        <div className= "flex flex-col py-10 gap-4">
-          {/*Labels*/}
-        </div>
+          <Doughnut {...config}></Doughnut>
 
+          <h3 className="mb-4 font-bold title">
+            Total Tasks
+            <span className="block text-3xl text-emerald-400">#(0)</span>
+          </h3>
+          <div className="flex flex-col py-10 gap-4">
+            {/*Labels*/}
+            <Labels></Labels>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
---
+++ frontend/src/components/IT22607232_Components/Labels.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Labels() {
+  return <div>Labels</div>;
+}
+
+function LabelComponent() {
+  return (
+    <div className="labels flex justify-between">
+      <div className="flex gap-2"></div>
+    </div>
+  );
+}
+
+
