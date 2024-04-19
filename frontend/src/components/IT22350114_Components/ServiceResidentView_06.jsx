8863bd901cf14a4d9e41e1a4fc20059956216693
Thihansi <thihansig@gmail.com>
2024-04-20 00:29:32 +0530

    css

--- frontend/src/components/IT22350114_Components/ServiceResidentView_06.jsx
+++ frontend/src/components/IT22350114_Components/ServiceResidentView_06.jsx
@@ -6,27 +6,13 @@ const ResidentServiceView = ({ service }) => {
 
 <div style={{ display: 'flex', justifyContent: 'center' }}>
 <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-200 bg-opacity-5 border-opacity-75 hover:bg-opacity-100">
-    <a href="#">
       <img className="p-8 rounded-t-lg" src={service.imageUrls} alt={service.serviceName} />
-    </a>
     <div className="px-5 pb-5">
-      <a href="#">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{service.serviceName}</h5>
-      </a>
 
       <div className="flex items-center mt-2.5 mb-5">
-        {/* Rating stars */}
-        <div className="flex items-center space-x-1 rtl:space-x-reverse">
-          {[...Array(4)].map((_, index) => (
-            <svg key={index} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-              <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-            </svg>
-          ))}
-          <svg className="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-          </svg>
-        </div>
-        <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0{service.serviceAvailability}</span> 
+        
+      <span className={`text-xs font-semibold px-2.5 py-0.5 rounded ms-3 ${service.serviceAvailability === 'Available' ? 'bg-green-100 text-green-800 dark:bg-green-200 dark:text-green-800' : 'bg-red-100 text-red-800 dark:bg-red-200 dark:text-red-800'}`}>{service.serviceAvailability}</span>
       </div> 
 
       <p className="text-gray-600 mt-2 mb-3 dark:text-white">{service.serviceDescription}</p>
