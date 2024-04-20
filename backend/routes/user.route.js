f2dcc6ba5b8553dfd43a8ebd2d122e6894b67401
Pradicksha <pradicksha1230@gmail.com>
2024-04-20 15:55:37 +0530

    changes made

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,14 +1,23 @@
-import express from 'express';
-import { deleteUser, getUserDetails, getUsers, signout, test, updateUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  deleteUser,
+  getUserDetails,
+  getUsers,
+  signout,
+  test,
+  updateUser,
+} from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
+import { getVisitorListings } from "../controllers/IT22561466_Controllers/visitorListing.controller.js";
 
 const router = express.Router();
 
-router.get('/test', test)
-router.put('/update/:userId', verifyToken, updateUser)
-router.delete('/delete/:userId', verifyToken, deleteUser)
-router.post('/signout', signout)
-router.get('/getusers', verifyToken, getUsers)
-router.get('/:id', verifyToken, getUserDetails);
+router.get("/test", test);
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
+router.get("/:id", verifyToken, getUserDetails);
+router.get("/visitorListings/:id", verifyToken, getVisitorListings);
 
 export default router;
