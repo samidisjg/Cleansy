413feca982cf0f84cfe5c5985974fadba4a02000
U-WarlockX <hewageuvindu@gmail.com>
2024-04-21 07:09:11 +0530

    new changes

--- frontend/src/pages/IT22196460_Pages/UpdateAnnouncementForm.jsx
+++ frontend/src/pages/IT22196460_Pages/UpdateAnnouncementForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const UpdateAnnouncementForm = () => {
     return (
         <div className="container mx-auto px-4 py-8">
-            <h2 className="text-3xl font-semibold mb-4">Create Announcement</h2>
+            <h2 className="text-3xl font-semibold mb-4">Update Announcement</h2>
             <form className="max-w-md mx-auto">
                 <div className="mb-4">
                     <label htmlFor="title" className="block text-gray-700 font-bold mb-2">Title</label>
@@ -26,12 +26,13 @@ const UpdateAnnouncementForm = () => {
                     <label htmlFor="title" className="block text-gray-700 font-bold mb-2">URL</label>
                     <input type="text" id="title" name="title" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
                 </div>
-                {/* <div className="mb-4">
+                
+                <div className="mb-4">
                     <label htmlFor="attachment" className="block text-gray-700 font-bold mb-2">Attachment</label>
                     <input type="file" id="attachment" name="attachment" className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
-                </div> */}
+                </div>
                 <div className="text-center">
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out hover:scale-105">Create Announcement</button>
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out hover:scale-105">Update Announcement</button>
                 </div>
             </form>
         </div>
